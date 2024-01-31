59e632d0fc9853306e5b3e00236dd1d8760b8a95
Hobbs <101003021+DerHobbs@users.noreply.github.com>
2024-01-31 04:53:19 +0100

    Version 1.0.0

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+-- Time in ms that is waited after the character has been selected. 1000 ms = 1 second.
+Config.CharSelected = 30000
+
+-- Texts that are displayed on the loading screen when the player is not in the Teamspeak ingame channel.
+Config.ScreenText1 = "You are not connected to Teamspeak!"
+Config.ScreenText2 = "or not in the Teamspeak ingame channel! check Teamspeak and your SaltyChat plugin immediately!"
+Config.ScreenText3 = "If you can't find a solution, please contact our support team!"
+
+Config.WaitBeforeCheck = 1000 -- Time in ms between the checks on CharacterSelected
+Config.IngameSaltyStatus = 2 -- The status value that represents 'Ingame' for SaltyChat
+Config.LoopWaitTime = 10000 -- Time in ms between the runs of the main loop
